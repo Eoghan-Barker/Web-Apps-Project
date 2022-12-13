24e358759d80d4180f347c7f466a7f00387297ae
Eoghan-Barker <g00397072@gmit.ie>
2022-12-13 14:50:57 +0000

    mongo table rendered in ejs view

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 var express = require("express");
 var app = express();
-let ejs = require("ejs");
-app.set('view engine', 'ejs');
+var ejs = require("ejs");
+var mongoDAO = require("./mongoDAO");
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   console.log("GET on /");
   //res.sendFile(__dirname + "/HomePage.html");
-  res.render('home');
+  res.render("home");
 });
 
 app.get("/employees", (req, res) => {
-    console.log("GET on /employees");
-    res.render('employees');
+  console.log("GET on /employees");
+  res.render("employees");
 });
 
 app.get("/departments", (req, res) => {
-    console.log("GET on /departments");
-    res.render('departments');
+  console.log("GET on /departments");
+  res.render("departments");
 });
 
 app.get("/employeesMongoDB", (req, res) => {
-    console.log("GET on /employeesMngoDB");
-    res.render('employeesMongoDB');
+  console.log("GET on /employeesMngoDB");
+
+  mongoDAO
+    .findAll()
+    .then((data) => {
+      res.render("employeesMongoDB", { employees: data });
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
 app.listen(3004, () => {
