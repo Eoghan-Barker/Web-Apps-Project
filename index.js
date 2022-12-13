02e93bafee7e8b137b5c4d7fbc956638d7941163
Eoghan-Barker <g00397072@gmit.ie>
2022-12-13 11:10:42 +0000

    added ejs views

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 var express = require("express");
 var app = express();
+let ejs = require("ejs");
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
   console.log("GET on /");
-  res.sendFile(__dirname + "/HomePage.html");
+  //res.sendFile(__dirname + "/HomePage.html");
+  res.render('home');
 });
 
 app.get("/employees", (req, res) => {
     console.log("GET on /employees");
+    res.render('employees');
 });
 
 app.get("/departments", (req, res) => {
     console.log("GET on /departments");
+    res.render('departments');
 });
 
 app.get("/employeesMongoDB", (req, res) => {
     console.log("GET on /employeesMngoDB");
+    res.render('employeesMongoDB');
 });
 
 app.listen(3004, () => {
